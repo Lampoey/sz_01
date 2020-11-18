ae609be33cce9fbce84e7646184671de12bb1831
Lampoey <Lampoey@163.com>
2020-11-18 19:50:34 +0800

    初始化

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,12 @@
+package com.itheima.pojo;
+
+/**
+ * 包名：com.itheima.pojo
+ *
+ * @Auther: Ou
+ * 日期: 2020/11/18/19:31
+ */
+public class User {
+    private String name;
+    private String password;
+}
